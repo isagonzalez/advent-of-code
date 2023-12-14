11eeec207f3dd46e825a3d362fef9f6282e64565
Isa Gonzalez <isaegonzalez03@gmail.com>
2023-12-13 22:05:47 -0500

    Day 1 Complete

--- 2023/day01/solution02.py
+++ 2023/day01/solution02.py
@@ -6,15 +6,11 @@ with open('input.txt', 'r') as file:
     nums = []
 
     for line in lines:
-        print(line.strip("\n"))
         spelled_nums_min = {line.find(digit): digit for digit in digits if line.find(digit) != -1}
         spelled_nums_max = {line.rfind(digit): digit for digit in digits if line.find(digit) != -1}
         spelled_nums = {**spelled_nums_min, **spelled_nums_max}
         min_index = min(spelled_nums.keys()) if spelled_nums else -1
         max_index = max(spelled_nums.keys()) if spelled_nums else -1
-
-        print(spelled_nums)
-        print("min:", min_index, "max:", max_index)
 
         length = len(line)
 
@@ -38,9 +34,7 @@ with open('input.txt', 'r') as file:
         num1 = digits[spelled_nums[min_index]] if first_index > min_index and min_index != -1 or first_index == -1 else first_num
         num2 = digits[spelled_nums[max_index]] if last_index < max_index and max_index != -1 or last_index == -1 else last_num
 
-        print("final:", num1, num2)
         nums.append(num1 + num2)
-        print()
 
     sum_nums = 0
 
